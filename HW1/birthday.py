fe86278d3ad80349850105c3e11c4743e59676c2
IsoNasir <iso.nasir3@gmail.com>
2021-06-16 18:51:06 -0500

    fix the code

--- HW1/birthday.py
+++ HW1/birthday.py
@@ -1,10 +1,4 @@
-# importing date class from datetime module
-from datetime import date
-# creating the date object of today's date
-todaysDate = date.today()
-# gets the current year 
-currentYear = todaysDate.year
-#once you run the code you as the user should enter your birth year.
+currentYear= 2021
 birthYear = input("Enter your birth year: ")
 
 age = currentYear - birthYear
